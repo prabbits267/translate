3a8072ad0afb94d1d57656a4d95c082c1b5487f1
rabbits123 <prabbit0167@gmail.com>
2018-11-13 05:27:31 +0700

    finish attention layer for batch training

--- seq2seq_batch/attn.py
+++ seq2seq_batch/attn.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 USE_CUDA = torch.cuda.is_available()
 
 # input : decoder_output, current hidden_state
-# output : attn energy (seq_len)
+# output : attn energy (batch, seq_len)
 class Attn(nn.Module):
     def __init__(self, method, hidden_size):
         super(Attn, self).__init__()
@@ -21,27 +21,53 @@ class Attn(nn.Module):
     # single sentence ht, hs
     # hidden (num_layers * num_directions, batch, hidden_size) (1, 32, 64)
     # encoder output (batch, time_step, hidden_size)            (32, max_timestep, 64) ==> batch, 64
+    # seq_len: array of max time step [<batch>]
     def forward(self, hidden, encoder_output):
         hidden = hidden.squeeze(0)
-        encoder_output = encoder_output.squeeze(0)
-        seq_len = len(encoder_output)
-        attn_energies = Variable(torch.zeros(seq_len))
+        batch_size = encoder_output.size(0)
+        max_seq_len = encoder_output.size(1)
+        attn_energies = Variable(torch.zeros(batch_size, max_seq_len))
         if USE_CUDA:
             attn_energies = attn_energies.cuda()
-        for i in range(seq_len):
-            attn_energies[i] = self.score(hidden, encoder_output[i])
+        # for i in range(seq_len):
+        #     attn_energies[i] = self.score(hidden, encoder_output[i])
+        # hidden[i]: [64],  encoder_output[i]:(max_timestep, 64)
+        # return (batch, max_len)
+        for i in range(batch_size):
+            attn_energies[i] = self.score(hidden[i], encoder_output[i])
         return F.softmax(attn_energies, dim=0)
 
-    # hidden (batch, 1, hidden_size) (batch, time_step, hidden_size)
+    # hidden (64)
+    # encoder_output: (max_time_step, 64)
+    # return max_time_step
     def score(self, hidden, encoder_output):
+        seq_len = encoder_output.size(0)
+        energy = torch.zeros([seq_len])
         if self.method == 'dot':
-            energy = torch.dot(hidden.view(-1), encoder_output.view(-1))
+            for i in range(seq_len):
+                energy[i] = torch.dot(hidden.view(-1), encoder_output[i].view(-1))
             return energy
+
         elif self.method == 'general':
-            energy = self.attn(encoder_output)
-            energy = torch.dot(hidden[0], energy)
-            return energy
-        elif self.method == 'concat':
-            energy = self.attn(torch.cat((hidden[0], encoder_output), 0))
-            energy = self.v.dot(energy)
-            return energy
+            for i in range(seq_len):
+                z = encoder_output[i]
+                energy_attn = self.attn(encoder_output[i])
+                energy[i] = torch.dot(hidden, energy_attn)
+                return energy
+        else:
+            for i in range(seq_len):
+                energy_attn = self.attn(torch.cat((hidden[0], encoder_output[i]), 0))
+                energy = self.v.dot(energy_attn)
+                return energy
+
+
+
+
+# attn = Attn('general', 64)
+# hidden = torch.randn(1, 32, 64)
+# encoder_output = torch.randn(32, 15, 64)
+# a = attn(hidden, encoder_output)
+# print(a)
+# print(a.size())
+
+
